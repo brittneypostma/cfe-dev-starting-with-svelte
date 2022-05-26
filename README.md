6f192f20adefa3588cfda675a6d3b3e74ab3681e
brittneypostma <bdesignedwebdev@gmail.com>
2022-05-26 13:36:37 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sveltejs-kit-template-default-myp16g
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sveltejs-kit-template-default-myp16g)
---
+++ src/routes/cats.svelte
@@ -0,0 +1,10 @@
+<script>
+export let cats;
+export let prerender = true
+</script>
+
+<h1>This is a group of cats</h1>
+
+{#each cats as cat}
+<img src={cat.url} alt="cat" />
+{/each}
